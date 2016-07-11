b87eb02319de816c7f3d98935766c1195960d362
Normand Cyr <normand.cyr@gmail.com>
2016-07-11 12:45:34 -0400

    optimize code for random word pick

--- generate-comment.py
+++ generate-comment.py
@@ -3,7 +3,6 @@ import random
 # list of popular English adjectives
 with open('dict/english-adjectives') as adjectivesfile:
     adjectives = adjectivesfile.read().split()
-
 # list of popular English nouns
 with open('dict/english-nouns') as nounssfile:
     nouns = nounssfile.read().split()
