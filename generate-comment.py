dab63dbb9a63663d1e27f715e7c8131afdc1eb24
Normand Cyr <normand.cyr@gmail.com>
2016-07-11 11:52:46 -0400

    remove CSV reading

--- generate-comment.py
+++ generate-comment.py
@@ -1,21 +1,20 @@
 import random
-import csv
 
-# list of 227 popular English adjectives
-with open('dict/english-adjectives.csv', 'rb') as adjectivesfile:
-    adjectivesreader = list(csv.reader(adjectivesfile, delimiter=','))
-lengthofadjectivefile = len(adjectivesreader)
+# list of popular English adjectives
+with open('dict/english-adjectives') as adjectivesfile:
+    adjectives = adjectivesfile.read().split()
+lengthofadjectivefile = len(adjectives)
 
-# list of 484 popular English nouns
-with open('dict/english-nouns.csv', 'rb') as nounssfile:
-    nounsreader = list(csv.reader(nounssfile, delimiter=','))
-lengthofnounsfile = len(nounsreader)
+# list of popular English nouns
+with open('dict/english-nouns') as nounssfile:
+    nouns = nounssfile.read().split()
+lengthofnounsfile = len(nouns)
 
 positionadj = random.randrange(0, lengthofadjectivefile)
-anadjective = adjectivesreader[positionadj]
+anadjective = adjectives[positionadj]
 positionnoun = random.randrange(0, lengthofnounsfile)
-anoun = nounsreader[positionnoun]
-twowords = " ".join(anadjective[0:1]) + " " + " ".join(anoun[0:1])
+anoun = nouns[positionnoun]
+twowords = anadjective + " " + anoun
 randomcomment = "What a " + twowords + " of an exercise dude!"
 
 print randomcomment
