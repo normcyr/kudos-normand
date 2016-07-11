7a91fc9d7f366c4175f61e1891492ba5e301dc7b
Normand Cyr <normand.cyr@gmail.com>
2016-07-11 12:44:37 -0400

    uptimize code for random word pick

--- generate-comment.py
+++ generate-comment.py
@@ -3,18 +3,12 @@ import random
 # list of popular English adjectives
 with open('dict/english-adjectives') as adjectivesfile:
     adjectives = adjectivesfile.read().split()
-lengthofadjectivefile = len(adjectives)
 
 # list of popular English nouns
 with open('dict/english-nouns') as nounssfile:
     nouns = nounssfile.read().split()
-lengthofnounsfile = len(nouns)
 
-positionadj = random.randrange(0, lengthofadjectivefile)
-anadjective = adjectives[positionadj]
-positionnoun = random.randrange(0, lengthofnounsfile)
-anoun = nouns[positionnoun]
-twowords = anadjective + " " + anoun
+twowords = random.choice(adjectives) + " " + random.choice(nouns)
 randomcomment = "What a " + twowords + " of an exercise dude!"
 
 print(randomcomment)
